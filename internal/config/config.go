67727c49d4149d56f45e812252aaffe6c4d404b4
gernima <braer.maks@gmail.com>
2025-10-14 22:57:32 +0300

    Init logger

---
+++ internal/config/config.go
@@ -0,0 +1,46 @@
+﻿package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env        string `yaml:"env" env-default:"development"`
+	HTTPServer `yaml:"http_server"`
+	Postgres   `yaml:"storage"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"5s"`
+}
+
+type Postgres struct {
+	Address  string `yaml:"address" env-default:"localhost:5432"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Database string `yaml:"database" env-default:"default"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatalf("CONFIG_PATH environment variable is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Cannot read config: %s", err)
+	}
+
+	return &cfg
+}
